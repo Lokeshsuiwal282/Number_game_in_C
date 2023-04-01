0e4bbf2705f4ed5db05182bda07e3c7b034844d3
Lokesh Kumar saini <116205481+Lokeshsuiwal282@users.noreply.github.com>
2023-04-01 17:58:48 +0530

    Add files via upload

---
+++ Numbergaming.c
@@ -0,0 +1,50 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+       int u;
+       int v,w,x,y,z;
+       int a=2000,b=2,c=999;
+
+   printf("My Name is Lokesh Kumar Saini");
+    printf("\n DJ Birol Nawalgrah");
+printf("\n Games of Number Total");
+printf("\n");
+printf("\n");
+printf("\n");
+for(int i=0;i<5;i++)
+{
+printf("Enter to 3 Digite number :");
+scanf("%d",&x);
+u=a+x-b;
+printf("Your value number total :%d",u);
+printf("\n");
+printf("\n");
+
+printf("Enter to 3 Digite number :%d",x);
+printf("\n");
+printf("Enter to 3 Digite number :");
+scanf("%d",&z);
+v=c-z;
+printf("Coumputer 3 digit number :%d",v);
+printf("\n");
+printf("Enter to 3 Digite number :");
+scanf("%d",&y);
+w=c-y;
+printf("Coumputer 3 digit number :%d",w);
+printf("\n");
+int e;
+e=z+x+v+y+w;
+printf("Your value number total :%d",e);
+printf("\n");
+printf("\n");
+   if(e==u)
+    {
+        printf("This value Total is same");
+    }
+printf("\n");
+printf("\n");
+}
+getch();
+}
